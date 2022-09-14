aff818114b09c3f6b5f1b25fe8699586918ec22a
luis <luisangel970930@gmail.com>
2022-09-13 23:13:01 -0400

    after swagger security token

--- src/main/java/com/main/ecasa/SwaggerConfiguration.java
+++ src/main/java/com/main/ecasa/SwaggerConfiguration.java
@@ -6,9 +6,16 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -16,10 +23,32 @@ public class SwaggerConfiguration {
     //http://localhost:8081/swagger-ui.html
 
 
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
     @Bean
     public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("RestAPI").apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.main.ecasa.controller")).paths(PathSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("RestAPI")
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.main.ecasa.controller"))
+                .paths(PathSelectors.any())
                 .build();
 
     }
